b1c1d8cdd2c8a9e89cda6fd40a3a0d62f880f137
BriukvinMaxim <briukvinmax@gmail.com>
2020-10-12 15:26:06 +0300

    03-gc: работа со сборщиками мусора

---
+++ 03-gc/src/main/java/ru/sbrf/cu/gc/Gc.java
@@ -0,0 +1,53 @@
+package ru.sbrf.cu.gc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Gc implements GcMBean {
+
+  private final int loopCounter;
+  private volatile int size = 0;
+
+  Gc(int loopCounter) {
+    this.loopCounter = loopCounter;
+  }
+
+  void run() throws InterruptedException {
+
+    // Для вызова OutOfMemory
+    Map<OOMObject, Integer> gcMap = new HashMap<>();
+    //Map<NormalObject, Integer> gcMap = new HashMap<>();
+
+    for (int i = 0; i < loopCounter; i++) {
+
+        /*
+          HashMap не позволяет использовать дубликаты ключей. В случае неверно определенного метода equals()
+          многочисленные объекты которые добавляются в качестве ключа будут накапливаться в HashMap и в конечном итоге приведут к OutOfMemory
+       */
+
+        // Benchmarking (очистка HashMap в каждом цикле)
+        // Map<OOMObject, Integer> gcMap = new HashMap<>();
+        //Map<NormalObject, Integer> gcMap = new HashMap<>();
+
+        for (int j = 0; j < size; j++) {
+          gcMap.put(new OOMObject("someField"), 1);
+          //gcMap.put(new NormalObject("someField"), 1);
+        }
+
+      //System.out.println(gcMap.size());
+      Thread.sleep(27500);
+    }
+  }
+
+  @Override
+  public int getSize() {
+    return size;
+  }
+
+  @Override
+  public void setSize(int size) {
+    System.out.println("new size:" + size);
+    this.size = size;
+  }
+
+}
